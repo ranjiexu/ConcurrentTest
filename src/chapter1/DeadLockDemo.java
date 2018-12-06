93c9638f96e9caf4383cc5f41ec4cc5e0af4386f
ranjiexu <ranjiexu@163.com>
2018-12-06 22:08:42 +0800

    死锁和多线程计算

---
+++ src/chapter1/DeadLockDemo.java
@@ -0,0 +1,47 @@
+package chapter1;
+
+/**
+ * Created by xuran on 2018/12/6.
+ */
+public class DeadLockDemo {
+
+    private static String A = "A";
+    private static String B = "B";
+
+    private void deadLock() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.currentThread().sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("thread1 ===");
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    synchronized (A) {
+                        System.out.println("thread2 ====");
+                    }
+                }
+
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+}
---
+++ src/chapter2/Counter.java
@@ -0,0 +1,63 @@
+package chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by xuran on 2018/12/6.
+ */
+public class Counter {
+    private AtomicInteger integer = new AtomicInteger(0);
+    private int i = 0;
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Counter counter = new Counter();
+        List<Thread> threadList = new ArrayList<>(200);
+
+        for (int i = 0; i < 100; i++) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j = 0; j < 10000; j++) {
+                        counter.count();
+                        counter.atomicCount();
+                    }
+                }
+            });
+            threadList.add(thread);
+        }
+
+        for (Thread thread : threadList) {
+            thread.start();
+        }
+
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        System.out.println("atomicInteger:" + counter.integer.get());
+        System.out.println("i:" + counter.i);
+        System.out.println("time:" + (System.currentTimeMillis() - start));
+    }
+
+    private void atomicCount() {
+        for (;;) {
+            int i = integer.get();
+            boolean suc = integer.compareAndSet(i, ++i);
+            if (suc) {
+                break;
+            }
+        }
+    }
+
+    private void count() {
+        i++;
+    }
+}
