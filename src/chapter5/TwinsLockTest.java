56603c816a6c3d5cf37d59e792bb07feda4944f3
ranjiexu <ranjiexu@163.com>
2018-12-18 21:55:59 +0800

    自定义同步器演示

---
+++ src/chapter5/TwinsLockTest.java
@@ -0,0 +1,40 @@
+package chapter5;
+
+import util.SleepUtils;
+
+/**
+ * Created by xuran on 2018/12/18.
+ * 自定义同步器
+ */
+public class TwinsLockTest {
+    public static void main(String[] args) {
+        TwinsLock lock = new TwinsLock();
+        class Worker extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        SleepUtils.second(1);
+                        System.out.println(Thread.currentThread().getName());
+                        SleepUtils.second(1);
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Worker worker = new Worker();
+            worker.setDaemon(true);
+            worker.start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            SleepUtils.second(1);
+            System.out.println();
+        }
+    }
+}
